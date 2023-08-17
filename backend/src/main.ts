9c3a35155cb4ab72c8526dc14607866ac688f27b
Rabbit523 <wisdom.gemini@outlook.com>
2023-08-16 23:50:00 -0500

    chore: enable cors into nest server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // This enables CORS for all origins, be careful in production
   await app.listen(4000);
 }
 bootstrap();
