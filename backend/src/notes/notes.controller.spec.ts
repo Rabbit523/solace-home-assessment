6a7d008d84342b1adcb4638c11f353f1709c02ed
Rabbit523 <wisdom.gemini@outlook.com>
2023-08-16 23:16:49 -0500

    feat: add tests for notes controller and service

--- backend/src/notes/notes.controller.spec.ts
+++ backend/src/notes/notes.controller.spec.ts
@@ -1,18 +1,56 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { NotesController } from './notes.controller';
+import { NotesService } from './notes.service';
 
 describe('NotesController', () => {
   let controller: NotesController;
+  let service: NotesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [NotesController],
-    }).compile();
-
-    controller = module.get<NotesController>(NotesController);
+    service = new NotesService(); // or mock it if needed
+    controller = new NotesController(service);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a note', () => {
+    const dto = { content: 'Test content' };
+    const note = controller.create(dto);
+    expect(note.content).toBe('Test content');
+  });
+
+  it('should retrieve all notes', () => {
+    controller.create({ content: 'Note 1' });
+    controller.create({ content: 'Note 2' });
+    const notes = controller.findAll();
+    expect(notes.length).toBe(2);
+  });
+
+  it('should retrieve a specific note using findOne', () => {
+    controller.create({ content: 'Sample Note 1' });
+    const note = controller.findOne('1'); // Note that id is passed as string due to @Param decorators
+    expect(note.id).toBe(1);
+    expect(note.content).toBe('Sample Note 1');
+  });
+
+  it('should update a specific note', () => {
+    controller.create({ content: 'Old Content' });
+    const updatedNote = controller.update('1', { content: 'Updated Content' });
+    expect(updatedNote.content).toBe('Updated Content');
+  });
+
+  it('should delete a note using remove', () => {
+    controller.create({ content: 'Sample Note 1' });
+    controller.remove('1');
+    expect(() => controller.findOne('1')).toThrow();
+  });
+
+  it('should find notes containing specific content using search', () => {
+    controller.create({ content: 'This is a sample note.' });
+    controller.create({ content: 'Another sample.' });
+    controller.create({ content: 'Irrelevant note.' });
+    const searchResults = controller.search('sample');
+    expect(searchResults.length).toBe(2);
   });
 });
--- backend/src/notes/notes.service.spec.ts
+++ backend/src/notes/notes.service.spec.ts
@@ -15,4 +15,44 @@ describe('NotesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a note', () => {
+    const note = service.create({ content: 'This is a sample note' });
+    expect(note.id).toBe(1);
+    expect(note.content).toBe('This is a sample note');
+  });
+
+  it('should retrieve all notes', () => {
+    service.create({ content: 'Sample Note 1' });
+    service.create({ content: 'Sample Note 2' });
+    const notes = service.findAll();
+    expect(notes.length).toBe(2);
+  });
+
+  it('should retrieve a specific note using findOne', () => {
+    service.create({ content: 'Sample Note 1' });
+    const note = service.findOne(1);
+    expect(note.id).toBe(1);
+    expect(note.content).toBe('Sample Note 1');
+  });
+
+  it('should update a specific note', () => {
+    service.create({ content: 'Old Content' });
+    const updatedNote = service.update(1, { content: 'Updated Content' });
+    expect(updatedNote.content).toBe('Updated Content');
+  });
+
+  it('should delete a note using remove', () => {
+    service.create({ content: 'Sample Note 1' });
+    service.remove(1);
+    expect(() => service.findOne(1)).toThrow();
+  });
+
+  it('should find notes containing specific content using search', () => {
+    service.create({ content: 'This is a sample note.' });
+    service.create({ content: 'Another sample.' });
+    service.create({ content: 'Irrelevant note.' });
+    const searchResults = service.search('sample');
+    expect(searchResults.length).toBe(2);
+  });
 });
